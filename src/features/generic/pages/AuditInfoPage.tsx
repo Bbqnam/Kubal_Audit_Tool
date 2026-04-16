f0d4740a8e6cbaa563f2803aa290b045d0910c54
Bbqnam <nguyen_phuong_nam@hotmail.com>
2026-04-16 07:53:45 +0200

    Update AuditInfoPage.tsx

--- src/features/generic/pages/AuditInfoPage.tsx
+++ src/features/generic/pages/AuditInfoPage.tsx
@@ -19,7 +19,6 @@ export default function GenericAuditInfoPage() {
     genericAuditInfo,
     updateAuditInfo,
     updateAuditTeam,
-    updateAuditStandard,
     updateAuditTitle,
     updateAuditRecord,
     changeAuditType,
